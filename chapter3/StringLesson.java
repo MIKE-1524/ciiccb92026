be06849dabc2b8422f36e00f4c7b7e333c3798d8
marcoyimyaem <marcoyimyaem@gmail.com>
2026-07-24 08:01:59 +0000

    Day 17 - Stringbuilder and array

--- chapter3/StringLesson.java
+++ chapter3/StringLesson.java
@@ -21,5 +21,28 @@ public class StringLesson {
         System.out.println(word1);
         String trimSample = "       \t \n \t Marc Yim          \t \n";
         System.out.println(trimSample.trim());
-    }
+        String names1 = "mabitag";
+        String names2 = "mabitag";
+        String names3 = "mabitag ";
+        //compile time
+        System.out.println("names1==names2 = "+(names1==names2));
+        System.out.println("names1==names3 = "+(names1==names3));
+        System.out.println("names2==names3 = "+(names2==names3));
+        //compile time
+        System.out.println("names1==names2 = "+(names1==names2));
+        System.out.println("names1==names3.trim() = "+(names1==names3.trim()));
+        System.out.println("names2==names3.trim() = "+(names2==names3.trim()));
+        //runtime
+        System.out.println("names1.equals(names2)= "+(names1.equals(names2)));
+        System.out.println("names2.equals(names3)= "+(names2.equals(names3)));
+        System.out.println("names1.equals(names3)= "+(names1.equals(names3)));
+
+        System.out.println("names1.equals(names2.trim())= "+(names1.equals(names2.trim())));
+        System.out.println("names2.equals(names3.trim())= "+(names2.equals(names3.trim())));
+        System.out.println("names1.equals(names3.trim())= "+(names1.equals(names3.trim())));
+        int x = 2;
+        int y = 1;
+        x*=y+1;
+        System.out.println(x);
+}
 }
